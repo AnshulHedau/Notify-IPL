319138101c0a6d5f397db6779b515b144e93a0f3
Anshul Hedau <anshulhedau01@gmail.com>
2018-04-13 22:17:34 +0530

    Test Commit

--- app.py
+++ app.py
@@ -52,28 +52,37 @@ def score():
 	period = forecast_items.find(class_="cb-lv-scrs-well")
 	if('cb-lv-scrs-col' in str(period)):
 		short_desc = period.find(class_="cb-lv-scrs-col").get_text()
-		short_cap = period.find(class_="cb-text-complete").get_text() 
+		#short_cap = period.find(class_="cb-text-complete").get_text() 
 		#print(short_desc)
 		#print(short_cap)
+		
+		team_playing = []
+    
 		list_item = short_desc.split('\xa0•\xa0')
 		list_name_score = list_item[0].split(' ')
 		scores_team_1 = []
 		scores_team_1.append(list_name_score[0])
 		scores_team_1.append(list_name_score[1])
 		scores_team_1.append(re.search('\(([^)]+)', str(list_item[0])).group(1))
+		team_playing.append(team_name[team.index(scores_team_1[0])])
 
 		scores_team_2 = []
-		list_name_score = list_item[0].split(' ')
-		if(len(list_name_score<2):
+		list_name_score = list_item[1].split(' ')
+		print(len(list_name_score))
+		if(len(list_name_score)>6):
 			scores_team_2.append(list_name_score[0])
 			scores_team_2.append(list_name_score[1])
-			scores_team_2.append(re.search('\(([^)]+)',str(list_item[1])).group(1))
-
-		team_playing = []
-		team_playing.append(team_name[team.index(scores_team_1[0])])
-		team_playing.append(team_name[team.index(scores_team_2[0])])
-
-		data = {"scores": {"teams" : team_playing,"team1" : scores_team_1,"team2" : scores_team_2,"status" : 10,"desc" : short_cap}}
+			scores_team_2.append(re.search('\(([^)]+)',str(list_item[1])).group(1))    
+			team_playing.append(team_name[team.index(scores_team_2[0])])
+		else:
+			scores_team_2.append(" ")
+			scores_team_2.append(" ")
+			scores_team_2.append(" ")    
+			team_playing.append(team_name[team.index(list_name_score[0])])
+			
+	
+	
+		data = {"scores": {"teams" : team_playing,"team1" : scores_team_1,"team2" : scores_team_2,"status" : 10}}
 
 	else:
 		teams_DATA = period.get('href')
